78273b5c7e681068e68edafb92eaf43998e225e6
Per Lejontand <per.lejontand@gmail.com>
2013-05-03 16:41:24 +0200

    Files was renamed

--- install.sql
+++ install.sql
@@ -3,11 +3,13 @@ CREATE DATABASE spartest;
 
 \c spartest
 
+CREATE LANGUAGE plperlu;
+
 \i TABLES/sparconfig.sql
 \i FUNCTIONS/http_post_xml.sql
 \i FUNCTIONS/format_spar_soap_envelope.sql
 \i FUNCTIONS/format_spar_identity.sql
 \i FUNCTIONS/format_spar_adress_query.sql
 \i FUNCTIONS/format_spar_personid_query.sql
-\i FUNCTIONS/parse_spar_personid_response.sql
+\i FUNCTIONS/parse_spar_response.sql
 \i populate.sql
