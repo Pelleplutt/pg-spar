3268b085c6c1e514354cefa62c721cd7d3b0fe62
Per Lejontand <per.lejontand@gmail.com>
2013-05-03 15:32:38 +0200

    Break down the main functions into smaller reusable parts; Verify functions with SPAR

--- FUNCTIONS/format_spar_personid_query.sql
+++ FUNCTIONS/format_spar_personid_query.sql
@@ -1,89 +1,53 @@
 CREATE OR REPLACE FUNCTION Format_SPAR_PersonID_Request(
 	_KundNr text,
 	_OrgNr text,
-	_UppdragsId text,
+	_SlutAnvandarId text,
+	_SlutAnvandarBehorighet text,
+	_SlutAnvandarSekretessRatt text,
 	_PersonID text
 	) RETURNS xml AS $BODY$
 DECLARE
+	identity xml;
+	soap_body xml;
+	soap_envelope xml;
+
 BEGIN
 
-RETURN xmlelement(
-	name "soap:Envelope",
-	xmlattributes(
-		'http://www.w3.org/2001/12/soap-envelope' AS "xmlns:soap",
-		'http://www.w3.org/2001/XMLSchema' AS "xmlns:xsd",
-		'http://www.w3.org/2001/XMLSchema-instance' AS "xmlns:xsi",
-		'http://www.w3.org/2001/12/soap-encoding' AS "soap:encodingStyle"
-	),
-	xmlelement(
-		name "soap:Body",
-		xmlelement(
+SELECT
+	*
+INTO
+	identity
+FROM
+	Format_SPAR_Identity(_KundNr, _OrgNr, _SlutAnvandarId, _SlutAnvandarBehorighet, _SlutAnvandarSekretessRatt);
+
+
+soap_body :=	xmlelement(
 			name "spain:SPARPersonsokningFraga",
 			xmlattributes(
 				'http://skatteverket.se/spar/komponent/1.0' AS "xmlns:spako",
 				'http://skatteverket.se/spar/instans/1.0' AS "xmlns:spain"
 			),
+			identity,
 			xmlelement(
-				name "IdentifieringsInformation",
-				xmlattributes(
-					'spako:IdentifieringsInformationTYPE' AS "xsi:type"
-				),
+				name "spako:PersonsokningFraga",
 				xmlelement(
-					name "KundNrLeveransMottagare", 
-					xmlattributes(
-						'spako:KundNrTYPE' AS "xsi:type"
-					),
-					_KundNr
-				),
-				xmlelement(
-					name "KundNrSlutkund", 
-					xmlattributes(
-						'spako:KundNrTYPE' AS "xsi:type"
-					),
-					_KundNr
-				),
-				xmlelement(
-					name "OrgNrSlutkund", 
-					xmlattributes(
-						'spako:OrgNrTYPE' AS "xsi:type"
-					),
-					_OrgNr
-				),
-				xmlelement(
-					name "UppdragsId", 
-					xmlattributes(
-						'spako:UppdragsIdTYPE' AS "xsi:type"
-					),
-					_UppdragsId
-				),
-				/*
-				xmlelement(
-					name "SlutAnvandarId", #FIXME (ej obl)
-					xmlattributes(
-						'spako:UppdragsIdTYPE' AS 'xsi:type',
+					name "spako:PersonId", 
+					xmlelement(
+						name "spako:FysiskPersonId", 
+						_PersonId
 					)
-				),
-				xmlelement(name "SlutAnvandarId", #FIXME _SlutAnvandarId),
-				xmlelement(name "SlutAnvandarBehorighet", #FIXME _SlutAnvandarBehorighet),
-				xmlelement(name "SlutAnvandarSekretessRatt", #FIXME _SlutAnvandarSekretessRatt),
-				*/
-				xmlelement(
-					name "Tidsstampel", 
-					xmlattributes(
-						'spako:TidsstampelTYPE' AS "xsi:type"
-					),
-					to_char(CURRENT_TIMESTAMP, 'YYYY-MM-DD"T"HH24:MI:SS.MS')
 				)
-			),
-			xmlelement(
-				name "PersonsokningFraga",
-				xmlattributes(
-					'spako:PersonsokningFragaTYPE' AS "xsi:type"
-				),
-				xmlelement(name "PersonId", _PersonId)
 			)
-		)
-	)
-);
+		);
+
+SELECT
+	*
+INTO
+	soap_envelope
+FROM
+	Format_SPAR_SOAP_Envelope(soap_body);
+
+RETURN soap_envelope;
+
 END;
 $BODY$ LANGUAGE plpgsql;
