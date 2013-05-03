30308c66858b4cd22575d4ad7a8b073646177947
Per Lejontand <per.lejontand@gmail.com>
2013-05-03 15:35:16 +0200

    Add extra fields needed for the test system, this will be WIP until we have final field list

--- TABLES/sparconfig.sql
+++ TABLES/sparconfig.sql
@@ -1,6 +1,8 @@
 CREATE TABLE SparConfig (
-	URL	text not null,
-	UppdragsID	text not null,
-	KundNr	text not null,
-	OrgNr	text not null
+	URL		text not null,
+	KundNr		text not null,
+	OrgNr		text not null,
+	SlutAnvandarId	text not null,
+	SlutAnvandarBehorighet text not null,
+	SlutAnvandarSekretessRatt text not null
 );
