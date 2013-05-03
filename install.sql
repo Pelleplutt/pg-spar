d7da5de94739df48ce8d3142ad24ef8b736689b1
Per Lejontand <per.lejontand@gmail.com>
2013-05-03 15:37:01 +0200

    Add all new files to install sql

--- install.sql
+++ install.sql
@@ -5,6 +5,9 @@ CREATE DATABASE spartest;
 
 \i TABLES/sparconfig.sql
 \i FUNCTIONS/http_post_xml.sql
+\i FUNCTIONS/format_spar_soap_envelope.sql
+\i FUNCTIONS/format_spar_identity.sql
+\i FUNCTIONS/format_spar_adress_query.sql
 \i FUNCTIONS/format_spar_personid_query.sql
 \i FUNCTIONS/parse_spar_personid_response.sql
 \i populate.sql
