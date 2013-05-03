520405536479c7fa4bc120f6205027bc32bec73c
Per Lejontand <per.lejontand@gmail.com>
2013-05-03 15:34:52 +0200

    Clean up a bit; Set content type header needed for SPAR

--- FUNCTIONS/http_post_xml.sql
+++ FUNCTIONS/http_post_xml.sql
@@ -1,32 +1,30 @@
 CREATE OR REPLACE FUNCTION HTTP_POST_XML(
 _URL text,
-_Username text,
-_Password text,
 _XML xml
 ) RETURNS xml AS $BODY$
 use strict;
 use warnings;
 use WWW::Curl::Easy;
 
-my ($url,$username,$password,$xml) = @_;
+my ($url,$xml) = @_;
 
-my @headers = ('SOAPAction: ""');
+my @headers = ('SOAPAction: ""', 'Content-type: text/xml; charset=utf-8');
 my $ch = new WWW::Curl::Easy;
-$ch->setopt(CURLOPT_HEADER,0);
-$ch->setopt(CURLOPT_HTTPHEADER,\@headers);
+$ch->setopt(CURLOPT_HEADER, 0);
+$ch->setopt(CURLOPT_HTTPHEADER, \@headers);
 $ch->setopt(CURLOPT_URL, $url);
-$ch->setopt(CURLOPT_USERNAME,$username);
-$ch->setopt(CURLOPT_PASSWORD,$password);
-$ch->setopt(CURLOPT_POST,1);
-$ch->setopt(CURLOPT_NOPROGRESS,0);
-$ch->setopt(CURLOPT_POSTFIELDS,$xml);
-$ch->setopt(CURLOPT_SSL_VERIFYPEER,1);
-$ch->setopt(CURLOPT_SSL_VERIFYHOST,2);
+$ch->setopt(CURLOPT_POST, 1);
+$ch->setopt(CURLOPT_NOPROGRESS, 0);
+$ch->setopt(CURLOPT_POSTFIELDS, qq#<?xml version="1.0" encoding="UTF-8"?># . $xml);
+$ch->setopt(CURLOPT_SSL_VERIFYPEER, 1);
+$ch->setopt(CURLOPT_SSL_VERIFYHOST, 2);
+$ch->setopt(CURLOPT_SSLCERT, "/home/per/ssl/978384C225B8B895.crt"); # FIXME 
+$ch->setopt(CURLOPT_SSLCERTTYPE, "PEM");#FIXME 
 
 my $response_body;
 
 open (my $fileb, ">", \$response_body);
-$ch->setopt(CURLOPT_WRITEDATA,$fileb);
+$ch->setopt(CURLOPT_WRITEDATA, $fileb);
 
 my $retcode = $ch->perform;
 
