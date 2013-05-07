9e90df2063ca49c4f6c6ece4b82ee37b4fdb9101
Per Lejontand <per.lejontand@gmail.com>
2013-05-07 10:21:24 +0200

    Cleanups

--- FUNCTIONS/http_post_xml.sql
+++ FUNCTIONS/http_post_xml.sql
@@ -1,14 +1,19 @@
 CREATE OR REPLACE FUNCTION HTTP_POST_XML(
-_URL text,
-_XML xml
-) RETURNS xml AS $BODY$
+		_URL text,
+		_XML xml
+	) RETURNS xml AS $BODY$
+
 use strict;
 use warnings;
 use WWW::Curl::Easy;
 
 my ($url,$xml) = @_;
 
-my @headers = ('SOAPAction: ""', 'Content-type: text/xml; charset=utf-8');
+my @headers = (
+		'SOAPAction: ""', 
+		'Content-type: text/xml; charset=utf-8'
+		);
+
 my $ch = new WWW::Curl::Easy;
 $ch->setopt(CURLOPT_HEADER, 0);
 $ch->setopt(CURLOPT_HTTPHEADER, \@headers);
@@ -29,12 +34,12 @@ $ch->setopt(CURLOPT_WRITEDATA, $fileb);
 my $retcode = $ch->perform;
 
 if ($retcode == 0) {
-    my $response_code = $ch->getinfo(CURLINFO_HTTP_CODE);
+	my $response_code = $ch->getinfo(CURLINFO_HTTP_CODE);
 } else {
-    my $curlerrortext = $ch->strerror($retcode);
-    die "ERROR_HTTP_POST_XML CURL_ERROR retcode: $retcode curlerrortext: $curlerrortext";
+	my $curlerrortext = $ch->strerror($retcode);
+	die "ERROR_HTTP_POST_XML CURL_ERROR retcode: $retcode curlerrortext: $curlerrortext";
 }
 
 return $response_body;
-$BODY$ LANGUAGE plperlu;
 
+$BODY$ LANGUAGE plperlu;
