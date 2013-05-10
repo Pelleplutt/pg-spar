2abd8883e75dfa23b6572221ea0e06de74e4bdd0
Per Lejontand <per.lejontand@gmail.com>
2013-05-10 13:51:27 +0200

    Remove populate values; They are just bogus anyhow; Rename response parsing function to be more generic

--- populate.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO SparConfig VALUES(
-        'https://XXXXXX.se/XXXXXXXXXXXXXXX/', 
-        'XXXXXX', 
-        'XXXXXXXXXX', 
-        'XXXXXXXXXXXXXX-XXXXXX',
-        'XXXX',
-        'N');
